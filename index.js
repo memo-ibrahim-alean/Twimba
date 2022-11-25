d5f12a8eb456624b65d500899bf8b416eace84f0
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-11-25 10:01:47 +0200

    Tweet Boilerplate

--- index.js
+++ index.js
@@ -2,7 +2,38 @@ import { tweetsData } from "./data.js";
 
 const tweetBtn = document.getElementById("tweet-btn");
 const tweetInput = document.getElementById("tweet-input");
+const tweetsFeed = document.getElementById("feed");
 
-tweetBtn.addEventListener('click', function() {
+tweetBtn.addEventListener("click", function () {
   console.log(tweetInput.value);
-})
+  getFeedHtml();
+});
+
+function getFeedHtml() {
+  let feedHtml = "";
+  for (let tweet of tweetsData) {
+    feedHtml += `
+      <div class="tweet">
+        <div class="tweet-inner">
+          <img src="${tweet.profilePic}" class="profile-pic">
+          <div>
+              <p class="handle">${tweet.handle}</p>
+              <p class="tweet-text">${tweet.tweetText}</p>
+              <div class="tweet-details">
+                  <span class="tweet-detail">
+                      ${tweet.replies.length}
+                  </span>
+                  <span class="tweet-detail">
+                      ${tweet.likes}
+                      </span>
+                  <span class="tweet-detail">
+                      ${tweet.retweets}
+                  </span>
+              </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+  tweetsFeed.innerHTML = feedHtml;
+}
